48e20aff64c88b89e70820d4c0bed0e1d91b41cc
TangyFish <92803369+TangyFish@users.noreply.github.com>
2022-04-26 08:32:36 -0500

    Add files via upload

--- medAsteroid.java
+++ medAsteroid.java
@@ -7,10 +7,19 @@ public class medAsteroid extends Prop {
 	public medAsteroid(int x, int y, int xVel, int yVel) {
 		super(x, y, xVel, yVel, new Polygon());
 		medAsts = new Polygon[3];
-		medAsts[0] = new Polygon(new int[] {0, 11, 36, 41, 21, 5, }, new int[] {16, 8, 7, 27, 36, 21, }, 6);
-		medAsts[1] = new Polygon(new int[] {5, 39, 48, 33, 14, 5, }, new int[] {28, 16, 36, 49, 46, 32, }, 6);
-		medAsts[2] = new Polygon(new int[] {0, 40, 54, 34, 14, 2,  }, new int[] {51, 32, 52, 71, 66, 52, }, 6);
+		medAsts[0] = new Polygon(new int[] {center.x+0, center.x+11, center.x+36, center.x+41, center.x+21, center.x+5, }, new int[] {center.y+16, center.y+8, center.y+7, center.y+27, center.y+36, center.y+21, }, 6);
+		medAsts[1] = new Polygon(new int[] {center.x+5, center.x+39, center.x+48, center.x+33, center.x+14, center.x+5, }, new int[] {center.y+28, center.y+16, center.y+36, center.y+49, center.y+46, center.y+32, }, 6);
+		medAsts[2] = new Polygon(new int[] {center.x+0, center.x+40, center.x+54, center.x+34, center.x+14, center.x+2,  }, new int[] {center.y+51, center.y+32, center.y+52, center.y+71, center.y+66, center.y+52, }, 6);
 		changeAsteroid();
+	}
+	
+	public void split() {
+		for (int i=0;i<3;i++) {
+			
+		}
+		int newVelx = (int)Math.cos(Math.toRadians(Math.random()*361));
+		int newVely = (int)Math.sin(Math.toRadians(Math.random()*361));
+		
 	}
 	
 	public void changeAsteroid() {
@@ -20,4 +29,3 @@ public class medAsteroid extends Prop {
 	
 
 }
-
