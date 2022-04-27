37b4854644ff26bd24e043ddfca6a9d29d863e46
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2022-04-27 08:21:38 -0500

    Add files via upload

--- medAsteroid.java
+++ medAsteroid.java
@@ -7,9 +7,9 @@ public class medAsteroid extends Prop {
 	public medAsteroid(int x, int y, int xVel, int yVel) {
 		super(x, y, xVel, yVel, new Polygon());
 		medAsts = new Polygon[3];
-		medAsts[0] = new Polygon(new int[] {center.x+0, center.x+11, center.x+36, center.x+41, center.x+21, center.x+5, }, new int[] {center.y+16, center.y+8, center.y+7, center.y+27, center.y+36, center.y+21, }, 6);
-		medAsts[1] = new Polygon(new int[] {center.x+5, center.x+39, center.x+48, center.x+33, center.x+14, center.x+5, }, new int[] {center.y+28, center.y+16, center.y+36, center.y+49, center.y+46, center.y+32, }, 6);
-		medAsts[2] = new Polygon(new int[] {center.x+0, center.x+40, center.x+54, center.x+34, center.x+14, center.x+2,  }, new int[] {center.y+51, center.y+32, center.y+52, center.y+71, center.y+66, center.y+52, }, 6);
+		medAsts[0] = new Polygon(new int[] {(int) (center.x+0), (int) (center.x+11), (int) (center.x+36), (int) (center.x+41), (int) (center.x+21), (int) (center.x+5), }, new int[] {(int) (center.y+16), (int) (center.y+8), (int) (center.y+7), (int) (center.y+27), (int) (center.y+36), (int) (center.y+21), }, 6);
+		medAsts[1] = new Polygon(new int[] {(int) (center.x+5), (int) (center.x+39), (int) (center.x+48), (int) (center.x+33), (int) (center.x+14), (int) (center.x+5), }, new int[] {(int) (center.y+28), (int) (center.y+16), (int) (center.y+36), (int) (center.y+49), (int) (center.y+46), (int) (center.y+32), }, 6);
+		medAsts[2] = new Polygon(new int[] {(int) (center.x+0), (int) (center.x+40), (int) (center.x+54), (int) (center.x+34), (int) (center.x+14), (int) (center.x+2),  }, new int[] {(int) (center.y+51), (int) (center.y+32), (int) (center.y+52), (int) (center.y+71), (int) (center.y+66), (int) (center.y+52), }, 6);
 		changeAsteroid();
 	}
 	
@@ -22,7 +22,7 @@ public class medAsteroid extends Prop {
 			angle = (int)Math.toDegrees(Math.atan(yVel/xVel));
 		}
 		
-		smallAsteroid split1 = new smallAsteroid(this);
+		//smallAsteroid split1 = new smallAsteroid(this);
 		
 		
 		for (int i=0;i<3;i++) {
